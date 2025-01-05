faa1a4d1d722e87827bd8385de755d307196df52
jlakhan1010 <jlakhan1010@gmail.com>
2025-01-05 18:42:39 +0530

    File exception handleing, Numpy

---
+++ PackageDemo/example.py
@@ -0,0 +1,5 @@
+from MyPackage.areafunctions import rectangle
+print ("Area :", rectangle(10,20))
+
+from MyPackage.mathfunctions import average
+print ("average:", average(10,20))
---
+++ PackageDemo/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mypackage',
+version='0.1',
+description='Package setup script',
+url='#',
+author='anonymous',
+author_email='test@gmail.com',
+license='MIT',
+packages=['mypackage'],
+zip_safe=False)
